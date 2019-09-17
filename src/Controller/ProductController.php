5856191bd754196add93a82df2da50e0da418d80
JasperSpillenaar <1033547@mborijnland.nl>
2019-09-17 18:10:31 +0200

    added mail function

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Form\CheckoutType;
 use App\Form\ProductType;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,6 +11,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Swift_Mailer;
+Use Swift_Message;
 
 
 /**
@@ -104,18 +107,16 @@ class ProductController extends AbstractController
     /**
      * @Route("/{id}/addToCart", name="addToCart", methods={"GET","POST"})
      */
-    public function addToCart ($id)
+    public function addToCart ($id): Response
 
-    {
-
-        $Cart = $this->session->get('Cart', []);
+    {        $Cart = $this->session->get('Cart', []);
         if (isset($Cart[$id]))
         {
             $Cart[$id]['array']++;
         } else {
             $Cart[$id] = array('array' => 1);
         }
-
+        $totaal = 0;
         $this->session->set('Cart', $Cart);
         ($this->session->get('Cart'));
         $cartArray = [];
@@ -123,10 +124,56 @@ class ProductController extends AbstractController
             $res = $this->getDoctrine()
                 ->getRepository(Product::class)
                 ->find($id);
+            array_push($cartArray, [$id, $product['array'], $res]);
+            $totaal = $totaal +($product['array'] * $res->getPrice());
         }
-        array_push($cartArray, [$id, $product['array'], $res]);
         return $this->render('product/addToCart.html.twig', [
-            'product' => $cartArray
+            'product' => $cartArray,
+            'totaal' => $totaal
+        ]);
+    }
+
+    /**
+     * @Route("/checkout", name="checkout",methods={"GET","POST"})
+     */
+    public function checkout (Request $request, Swift_Mailer $mailer): Response
+
+    {
+        $form = $this->createForm(CheckoutType::class);
+        $form->handleRequest($request);
+
+        $Cart = $this->session->get('Cart', []);
+
+        if($form->isSubmitted() && $form->isValid()){
+//            $name = $form->get('name')->getData();
+//            $email = $form->get('email')->getData();
+
+            $message = (new Swift_Message('Factuur'))
+                ->setFrom('jspillenaar@gmail.com')
+                ->setTo('jspillenaar@gmail.com')
+                ->setBody('Welcome to Maitrap!</p>Now your test emails will be <i>safe</i> ' .  . $Cart['name'],  'text/html');
+            $mailer->send($message);
+            $this->session->clear();
+            return $this->redirect('/');
+        }
+
+        $Cart = $this->session->get('Cart', []);
+
+        $totaal = 0;
+        $this->session->set('Cart', $Cart);
+        ($this->session->get('Cart'));
+        $cartArray = [];
+        foreach ($Cart as $id => $product) {
+            $res = $this->getDoctrine()
+                ->getRepository(Product::class)
+                ->find($id);
+            array_push($cartArray, [$id, $product['array'], $res]);
+            $totaal = $totaal +($product['array'] * $res->getPrice());
+        }
+        return $this->render('product/checkout.html.twig', [
+            'form' => $form->createView(),
+            'product' => $cartArray,
+            'totaal' => $totaal
         ]);
     }
 }
